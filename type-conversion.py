b55205dacb13de0325a2fdb71da396bd1b940fc5
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-01 11:49:15 +0530

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,83 @@
+# a='ravi'
+# b=4
+# c='python'
+# d=7.0
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(type(a))  #'str'
+# print(type(b))  #'int'
+# print(type(c))# 'str'
+# # print(type(d))   #'float'
+# a=6
+# print(type(a))   #6
+# print(float(a))  #6.0
+# print(str('a'))  ''' int to str not possibl '''
+# a=2; b='python'
+# print(a)   #2
+# print(b)   #python 
+# print(type(a))  #'int'
+# print(type(b))   #'str'
+# print(a+b)
+# print(b+a)
+# print('python'+2) unsupported
+# print(2+'python')  un supported 
+# print('python'+'2') #python2  'str'+'int'
+# print('2'+'python') #2python   'int'+'str'
+# print('python'+2)    
+# print('python'+'2.0')  #python2.0  'str'+'float'
+# print('2.0'+'python')  #2.0python   'float'+'float'
+# a=6; b='ravi'
+# rint(str(a)+b)
+# print(b+str(a))p
+# print(int(a)+b)
+# print(b+int(int)) not supported
+# x='samsung'; y='m'; z='redme'
+# print(x+y+z)  # samsungmredme
+# print(x+''+y+z)  #samsungmredme
+# x='samsung'; y='redme'; z=31
+# # print(x+y+z)
+# print(x+y+str(z)) #samsungredme31
+# print(x+y+'-'+str(z))  #samsungredne-31
+# u=2.0
+# print(int(u))   #float to convert to int  2 
+# print(str(u))   #float to convert to str  2.0
+# a='hello'
+# # '''print(type(a))   #str'''  this is interview question also
+# print(int(a))
+# print(float(a))
+# b='8'      # str
+# print(type(b)) 
+# print(int(b))  #8
+# print(float(b))  #8.0
+# c=7.2   float
+# print(c)    #7.2
+# print(str(c))   #7.2
+# print(float(c))   #7.2
+# print(int(c))     #7.2
+# s='7.8'
+# # print(type(s))  #7.8
+# # # print(s)   #str
+# # print(int(s))  # value error
+# print(float(s))   #7.8
+# print(str(s))      #7.8
+# a='hello'
+# b='world'
+# c=123
+# print(a+'-'+b+str(c))
+# 'hellopython'
+# print('hello python')
+# print("hello python")
+# print('''hello python''')
+# print("""hello python""")
+# print('helo','python')
+# print("hello","python")
+# print('hello'+''+'python')
+# print('b','u')
+# print('hello'+'     '+'python')
+# print('pravalika'*'niharika') # type error
+# print('hello'*7)    #type error
+# print('hello'*7.0) #type error
+# print('hi'*3)
+# print('h'*3)
